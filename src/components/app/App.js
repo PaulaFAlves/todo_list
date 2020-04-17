c1c7a19a6bb07a75e59dae78b6b6b157637599eb
Paula Alves <paulafernandaalves@icloud.com>
2020-04-17 08:18:53 -0300

    organized the components

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return(
+			<div className="main">
+				<header>Things todo</header>
+				<form className="toDoForm">
+					<input type="text" placeholder="Write in here!" />
+					<button type="submit">Add</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+
